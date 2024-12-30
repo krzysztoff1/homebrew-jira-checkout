be068e2fcbce66e069db54fea952e1ae6168b966
Krzysztof Duda <duda_krzysztof@outlook.com>
2024-12-30 16:00:10 +0100

    homebrew-jira-checkout

--- Formula/jira_checkout.rb
+++ Formula/jira_checkout.rb
@@ -1,9 +1,9 @@
 class JiraCheckout < Formula
   desc 'Checkout to branch from Jira'
-  homepage 'https://github.com/krzysztoff1/jira-checkout-rb'
+  homepage 'https://github.com/krzysztoff1/homebrew-jira-checkout'
   version '1.0.0'
 
-  url 'https://github.com/krzysztoff1/jira-checkout-rb/archive/main.zip', :using => :curl
+  url 'https://github.com/krzysztoff1/homebrew-jira-checkout/archive/main.zip', :using => :curl
 
   def install
     bin.install 'bin/jira_checkout'
